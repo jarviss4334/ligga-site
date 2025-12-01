db1d3b55090b29747e48474558e0213bb5412606
Thejus S <thejussasikumar80@gmail.com>
2025-12-01 09:52:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require("path");
 // Serve static website files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Ping route for cronjob monitoring 
+app.get("/ping", (req, res) => {
+  res.send("Server is alive ✅");
+});
+
 // Use Render's port or fallback to 3000 for local testing
 const PORT = process.env.PORT || 3000;
 
